65ff7c8a5b43d7a20332d7fcb90eae71374bd61c
2015180012박건호 <ehdrpakt@naver.com>
2018-09-18 17:44:55 +0900

    색다른진도
    오늘 수업진도대신 나간  색다른진도

---
+++ Drill5/Drill-05.py
@@ -0,0 +1,17 @@
+from pico2d import *
+import math
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+while True:
+    make_rectangle()
+    make_circle()
+    
+
+
+
+    
+
+close_canvas()
