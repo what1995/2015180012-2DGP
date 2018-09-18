1ab19600c3df0be83ff75ddcfeae8ad227cdbdb9
2015180012박건호 <ehdrpakt@naver.com>
2018-09-18 18:41:02 +0900

    10개의 좌표점 이동함수
    10개의 좌표점으로 각각 이동하는 10개의 함수를 만들었다.

--- Drill5/Drill-05.py
+++ Drill5/Drill-05.py
@@ -1,10 +1,39 @@
 from pico2d import *
-import math
+
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
-
-
+character = load_image('character_sheet.png')
+def character_1st_move():
+     pass
+def character_2nd_move():
+    pass
+def character_3rd_move():
+    pass
+def character_4th_move():
+    pass
+def character_5th_move():
+    pass
+def character_6th_move():
+    pass
+def character_7th_move():
+    pass
+def character_8th_move():
+    pass
+def character_9th_move():
+    pass
+def character_10th_move():
+    pass
+while True:
+    character_1st_move()
+    character_2nd_move()
+    character_3rd_move()
+    character_4th_move()
+    character_5th_move()
+    character_6th_move()
+    character_7th_move()
+    character_8th_move()
+    character_9th_move()
+    character_10th_move()
 
 close_canvas()
