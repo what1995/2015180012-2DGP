b126e7ec01c21c75f1b754d205455c7aff172371
2015180012박건호 <ehdrpakt@naver.com>
2018-09-19 20:41:36 +0900

    마우스 좌표를 따라 캐릭터이동
    마우스의 x좌표를 따라 캐릭터는 이동을하나 아직 y쪽 좌표를 받아오지못하고있다

--- Drill6/Drill6.py
+++ Drill6/Drill6.py
@@ -1,11 +1,8 @@
 from pico2d import *
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+KPU_WIDTH, KPU_HEIGHT = 800, 600
 
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
-kpu_ground = load_image('KPU_GROUND.png')
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
-Mouse = load_image('hand_arrow.png')
+
+
 
 running = True
 x = 0
@@ -13,13 +10,27 @@ frame = 0
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type==SDL_QUIT:
+            running = False
+        elif event.type ==SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT -1 - event.y
+        elif event.type==SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
+            running = False
 
 
-while x < 800 and running:
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+Mouse = load_image('hand_arrow.png')
+
+while x < KPU_WIDTH and running:
     clear_canvas()
-    grass.draw(400, 30)
+    kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
     character.clip_draw(frame * 100, 100, 100, 100, x, 90)
     update_canvas()
 
