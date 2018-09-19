ee98e3885bec433630db1369c6ecf075b4837f36
2015180012박건호 <ehdrpakt@naver.com>
2018-09-19 20:45:16 +0900

    마우스 커서 완성
    마우스 커서를 완성했으나 손가락끝이 마우스커서가 아니고 그림의 중심이 마우스커서라서 수정이필요하다

--- Drill6/Drill6.py
+++ Drill6/Drill6.py
@@ -5,19 +5,20 @@ KPU_WIDTH, KPU_HEIGHT = 800, 600
 
 
 running = True
-x = 0
+M_x = 0
+M_y = 0
 frame = 0
 
 
 def handle_events():
     global running
-    global x, y
+    global M_x, M_y
     events = get_events()
     for event in events:
         if event.type==SDL_QUIT:
             running = False
         elif event.type ==SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT -1 - event.y
+            M_x, M_y = event.x, KPU_HEIGHT - event.y
         elif event.type==SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
             running = False
 
@@ -28,15 +29,14 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 Mouse = load_image('hand_arrow.png')
 
-while x < KPU_WIDTH and running:
+while M_x < KPU_WIDTH and running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    Mouse.clip_draw(0, 0, 100, 100, M_x, M_y)
     update_canvas()
 
     handle_events()
-    frame = (frame + 1) % 8
-    x += 5
+
     delay(0.05)
 
 close_canvas()
