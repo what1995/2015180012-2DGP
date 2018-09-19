852e46055976435cba6ecf58428153d0780b0a34
2015180012박건호 <ehdrpakt@naver.com>
2018-09-19 21:38:05 +0900

    마우스 클릭좌표로 캐릭터이동
    이제 클릭하면 클릭한곳으로 캐릭터가 애니매이션으로 이동한다.

--- Drill6/Drill6.py
+++ Drill6/Drill6.py
@@ -3,19 +3,24 @@ KPU_WIDTH, KPU_HEIGHT = 800, 600
 
 
 
-
+moving = True
 running = True
 M_x = 0
 M_y = 0
 C_x = KPU_WIDTH//2
 C_y = KPU_HEIGHT//2
+C_M_x = 0
+C_M_y = 0
 frame = 0
-
+cheak=0
 
 def handle_events():
     global running
     global M_x, M_y
     global C_x, C_y
+    global C_M_x, C_M_y
+    global moving
+    global  cheak
     events = get_events()
     for event in events:
         if event.type==SDL_QUIT:
@@ -25,7 +30,16 @@ def handle_events():
         elif event.type==SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
             running = False
         elif event.type==SDL_MOUSEBUTTONDOWN:
-            C_x, C_y= event.x , KPU_HEIGHT - event.y
+            moving=True
+            cheak=0
+            C_M_x, C_M_y = (event.x - C_x) / 10, (KPU_HEIGHT - event.y - C_y) / 10
+
+
+
+
+
+
+
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -42,7 +56,18 @@ while M_x < KPU_WIDTH and running:
 
     character.clip_draw(frame * 100, 100, 100, 100, C_x, C_y)
     frame = (frame + 1) % 8
+    if moving == True:
+        C_x += C_M_x
+        C_y += C_M_y
+        cheak = cheak + 1
+    if cheak ==10:
+        moving=False
+        cheak=0
+
+
+
     update_canvas()
+
 
     handle_events()
     delay(0.05)
