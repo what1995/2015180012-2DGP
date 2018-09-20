af7d2123e8a77dd60e9d521e3274b405ae6fae37
2015180012박건호 <ehdrpakt@naver.com>
2018-09-20 18:32:08 +0900

    수업내용관련자료
    키보드 이동 마우스이동 esc 종료 실습을하였다.

--- Drill6/move_character_with_key.py
+++ Drill6/move_character_with_key.py
@@ -2,8 +2,36 @@ from pico2d import *
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    global x
+    global dir
+    global cheak
+    events = get_events()
+    for event in events:
+        if event.type ==SDL_QUIT:
+            running = False
+        elif event.type==SDL_KEYDOWN:
+            if event.key ==SDLK_RIGHT:
+                dir +=1
+                cheak =1
+
+            elif event.key ==SDLK_LEFT:
+                dir -=1
+                cheak = -1
+
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+                cheak = 0
+
+            elif event.key == SDLK_LEFT:
+                dir +=1
+                cheak = 0
+
+
+
 
 
 open_canvas()
@@ -13,16 +41,22 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
-
+dir = 0
+cheak =0
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    if cheak ==1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    elif cheak == -1:
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    elif cheak ==0:
+        character.clip_draw(300, 0, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
-
+    x +=dir*5
     delay(0.05)
 
 close_canvas()
