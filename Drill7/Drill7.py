da0bd8f322f5022da374dd63fc8677c6de6ee69e
2015180012박건호 <ehdrpakt@naver.com>
2018-10-02 18:51:16 +0900

    20개의 랜덤점을 받아오는 list작성
    20개의 랜덤점을 받아오는 list작성하였다

--- Drill7/Drill7.py
+++ Drill7/Drill7.py
@@ -12,11 +12,27 @@ def character_draw(p1, p2):
     
 
     for i in range(0, 100, 10):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH//2, KPU_HEIGHT//2)
         t= i/100
         x= (1-t)*p1[0]+t*p2[0]
         y= (1-t)*p1[1]+t*p2[1]
-        
 
+        update_canvas()
+
+        delay(0.1)
+
+size = 20
+points =[(random.randint(0, 800), random.randint(0, 600)) for i in range(size)]
+
+n = 1
+while True:
+
+    character_draw(points[n-1], points[n])
+    n = (n+1) % size
+
+
+close_canvas()
 
 
 
