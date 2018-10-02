55e3b4aab86817dc0c768525df200d3b56417a78
2015180012박건호 <ehdrpakt@naver.com>
2018-10-02 18:52:26 +0900

    캐릭터를 구현하였다
    이제 20개의 랜덤점을 통해 캐릭터가 이동한다

--- Drill7/Drill7.py
+++ Drill7/Drill7.py
@@ -17,10 +17,12 @@ def character_draw(p1, p2):
         t= i/100
         x= (1-t)*p1[0]+t*p2[0]
         y= (1-t)*p1[1]+t*p2[1]
-
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
         update_canvas()
+        frame = (frame + 1) % 8
 
         delay(0.1)
+    character.clip_draw(frame * 100, 100, 100, 100, p2[0], p2[1])
 
 size = 20
 points =[(random.randint(0, 800), random.randint(0, 600)) for i in range(size)]
