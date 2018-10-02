df92a81fdb52dd52cff5fd4226f2682d7ffee805
2015180012박건호 <ehdrpakt@naver.com>
2018-10-02 18:47:51 +0900

    점이동 함수제작
    수업에 배운내용대로 벡터를이용한 점의 이동함수를 구현

--- Drill7/Drill7.py
+++ Drill7/Drill7.py
@@ -5,3 +5,18 @@ KPU_WIDTH, KPU_HEIGHT = 800, 600
 character = load_image('animation_sheet.png')
 kpu_ground = load_image('KPU_GROUND.png')
 
+def character_draw(p1, p2):
+    frame =0
+    dir =0
+
+    
+
+    for i in range(0, 100, 10):
+        t= i/100
+        x= (1-t)*p1[0]+t*p2[0]
+        y= (1-t)*p1[1]+t*p2[1]
+        
+
+
+
+
