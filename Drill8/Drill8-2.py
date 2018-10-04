afd33ed9a6209bbff4f5bc575aa25f15edcc6879
2015180012박건호 <ehdrpakt@naver.com>
2018-10-04 18:47:10 +0900

    랜덤점을따라 캐릭터가 이동한다
    랜덤점을따라 캐릭터가 이동한다

--- Drill8/Drill8-2.py
+++ Drill8/Drill8-2.py
@@ -8,12 +8,27 @@ kpu_ground = load_image('KPU_GROUND.png')
 
 
 def character_draw(p1, p2,p3, p4):
-    
+    frame = 0
+    dir = 1
+
+    for i in range(0, 100, 10):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
+
+        if (dir == -1):
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        if (dir == 1):
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.1)
 
 
-
-
-    pass
 
 
 
