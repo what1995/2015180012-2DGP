5f6a417fcd197e18510be897301e3844c08481cc
2015180012박건호 <ehdrpakt@naver.com>
2018-10-04 19:18:52 +0900

    스탬프찍기
    스탬프를찎었다

--- Drill8/Drill8-2.py
+++ Drill8/Drill8-2.py
@@ -6,19 +6,30 @@ character = load_image('animation_sheet.png')
 kpu_ground = load_image('KPU_GROUND.png')
 
 
-
+def character_stam():
+    global c
+    if (c > 9):
+        c = 9
+    for j in range(0, c + 1):
+        positon = points[j]
+        character.clip_draw(100, 100, 100, 100, positon[0], positon[1])
 def character_draw(p1, p2,p3, p4):
     frame = 0
     dir = 1
+    global c
     if ((p2[0] - p1[0]) > 0):
         dir = -1
     elif ((p2[0] - p1[0]) < 0):
         dir = 1
 
 
+
+
     for i in range(0, 100, 10):
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character_stam()
+
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
@@ -27,6 +38,8 @@ def character_draw(p1, p2,p3, p4):
             character.clip_draw(frame * 100, 100, 100, 100, x, y)
         if (dir == 1):
             character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.1)
@@ -49,11 +62,16 @@ def character_draw(p1, p2,p3, p4):
 
 
 
+
+
 size = 10
 points =[(random.randint(0, 800), random.randint(0, 600)) for i in range(size)]
 n=3
+c=0
 while True:
     character_draw(points[n - 3], points[n-2],points[n-1],points[n])
+
     n = (n + 1) % size
+    c=c+1
 
 close_canvas()
