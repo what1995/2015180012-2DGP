dfd7878eb02f9a772b3a26085e56e1ea1d8d07a8
2015180012박건호 <ehdrpakt@naver.com>
2018-10-04 18:31:52 +0900

    Drill8-2 시작
    Drill8-2를 시작하였다

--- Drill8/Drill8-2.py
+++ Drill8/Drill8-2.py
@@ -4,3 +4,30 @@ open_canvas()
 KPU_WIDTH, KPU_HEIGHT = 800, 600
 character = load_image('animation_sheet.png')
 kpu_ground = load_image('KPU_GROUND.png')
+
+
+
+def character_draw(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+close_canvas()
