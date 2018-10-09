e2c7d0319931ae4aab22d0eaf5e328d1a3f0dd3e
2015180012박건호 <ehdrpakt@naver.com>
2018-10-09 18:47:42 +0900

    Drill9 완성
    2015180012 박건호 Drill9완성
    Commit횟수 7번
    https://youtu.be/cnViFmNs7h4

--- Drill9/Drill9.py
+++ Drill9/Drill9.py
@@ -1,5 +1,17 @@
 from pico2d import *
 import random
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100,700),90
+        self.frame =random.randint(0,7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame+1)%8
+        self.x +=5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
 
 class Grass:
     def __init__(self):
@@ -45,6 +57,7 @@ def handle_events():
 open_canvas()
 grass = Grass()
 big = BigBall()
+team = [Boy() for i in range(11)]
 small=SmallBall()
 running = True
 
@@ -59,6 +72,10 @@ while running:
 
     clear_canvas()
     grass.draw()
+    for boy in team:
+        boy.draw()
+        boy.update()
+
     for big in bigteam:
         big.draw()
         big.update()
