ddd57c8d67d52a226aa85ff30725f4a5825c80c9
2015180012박건호 <ehdrpakt@naver.com>
2018-10-16 23:00:19 +0900

    마리사 스킬모션 완료
    모션완료

--- Project/Player.py
+++ Project/Player.py
@@ -82,6 +82,8 @@ class Player:
         self.LastspellEframe1 = 0
 
     def update(self):
+
+        #####STANDING#########
         if self.stat==40:
             if self.Standcheak < 10:
                 self.Standi = (self.Standi+1) % 11
@@ -91,6 +93,8 @@ class Player:
                 self.Standi = 0
                 self.Standj= 0
                 self.Standcheak = 0
+
+        #####SKILL1#########
         if self.stat ==41:
             if self.Skill1cheak < 18:
                 if self.Skill1cheak < 6:
@@ -111,6 +115,53 @@ class Player:
                 self.Skill1cheak = 0
                 self.Skill1Eframe1 = 0
                 player.stat = 40
+
+        #####SKILL2#########
+        if self.stat ==42:
+            if self.Skill2cheak < 7:
+                self.Skill2i = (self.Skill2i + 1) % 8
+                self.Skill2j = (self.Skill2j + 1) % 7
+                # Player
+                self.MS2Effect.clip_draw(0, 125, 132, 125, self.Player + self.Skill2Ex1, self.All_Y)
+                self.MS2Effect.clip_draw(132, 125, 132, 125, self.Player + self.Skill2Ex2, self.All_Y)
+                self.MS2Effect.clip_draw(264, 125, 132, 125, self.Player + self.Skill2Ex3, self.All_Y)
+                self.Skill2Ex1 += 75
+                self.Skill2Ex2 += 75
+                self.Skill2Ex3 += 75
+                self.Skill2cheak += 1
+
+            if self.Skill2cheak == 7:
+                self.Skill2i = 0
+                self.Skill2j = 0
+                self.Skill2Ex1 = 120
+                self.Skill2Ex2 = 100
+                self.Skill2Ex3 = 80
+                self.Skill2cheak = 0
+                player.stat=40
+
+        #####SKILL3#########
+        if self.stat==43:
+            if self.Skill3cheak < 17:
+                if self.Skill3cheak < 7:
+                    self.Skill3i = (self.Skill3i + 1) % 10
+                    self.Skill3j = (self.Skill3j + 1) % 10
+                if self.Skill3cheak >= 7:
+                    self.MS3Effect.clip_draw(self.Skill3Eframe1 * 260, 255, 260, 255, self.Player + self.Skill3Ex1,self.All_Y)
+                    self.Skill3Eframe1 = (self.Skill3Eframe1 + 1) % 3
+                    self.Skill3Ex1 += 80
+                if self.Skill3cheak >= 13:
+                    self.Skill3i = (self.Skill3i + 1) % 10
+                    self.Skill3j = (self.Skill3j + 1) % 10
+                self.Skill3cheak += 1
+
+            if self.Skill3cheak == 17:
+                self.Skill3i = 0
+                self.Skill3j = 0
+                self.Skill3Ex1 = 120
+                self.Skill3cheak = 0
+                player.stat=40
+
+        #####LASTSPELL#########
         if self.stat==44:
             if self.Lastspellcheak < 18:
                 self.Lastspelli = (self.Lastspelli + 1) % 17
@@ -135,18 +186,36 @@ class Player:
 
 
     def Stand(self):
+
+        ####STANDING########
         if self.stat==40:
             self.Standframe1 = [0,65,126,188,250,312,372,434,495,556,618]
             self.Standframe2 = [65,61,62,62,62,60,62,61,60,62]
           #플레이어
             self.MStanding.clip_draw(self.Standframe1[self.Standi], 110, self.Standframe2[self.Standj], 110, self.Player,self.All_Y )
-
+        #####SKILL1#########
         if self.stat==41:
             self.Skill1frame1 = [0, 71, 132, 197, 262, 322, 396, 468, 536, 600]
             self.Skill1frame2 = [71, 61, 65, 65, 58, 72, 72, 66, 60]
             # 플레이어
             self.MSkill1.clip_draw(self.Skill1frame1[self.Skill1i], 105, self.Skill1frame2[self.Skill1j], 105,self.Player, self.All_Y)
+        #####SKILL2#########
+        if self.stat==42:
+            self.Skill2frame1 = [0, 85, 165, 240, 318, 395, 464, 525]
+            self.Skill2frame2 = [85, 80, 75, 78, 76, 67, 64]
 
+            # 플레이어
+            self.MSkill2.clip_draw(self.Skill2frame1[self.Skill2i], 120, self.Skill2frame2[self.Skill2j], 120,self.Player, self.All_Y)
+
+        #####SKILL3#########
+        if self.stat ==43:
+            self.Skill3frame1 = [0, 65, 125, 195, 275, 332, 412, 500, 590, 661]
+            self.Skill3frame2 = [65, 60, 70, 80, 60, 76, 85, 89, 68, 61]
+
+            # 플레이어
+            self.MSkill3.clip_draw(self.Skill3frame1[self.Skill3i], 110, self.Skill3frame2[self.Skill3j], 110,self.Player, self.All_Y)
+
+        #####LASTSPELL#########
         if self.stat == 44:
             self.Lastframe1 = [0, 65,127,187,251,305,386,451,536,610,680,750,815,880,943,1010,1070]
             self.Lastframe2 = [65,62,60,64,55,79,62,80,72,66,66,65,63,60,59,58,58]
@@ -165,6 +234,10 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_a:
             player.stat = 41
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_s:
+            player.stat = 42
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_d:
+            player.stat = 43
         elif event.type == SDL_KEYDOWN and event.key == SDLK_f:
             player.stat = 44
         elif event.type == SDL_KEYDOWN and event.key == SDLK_1:
