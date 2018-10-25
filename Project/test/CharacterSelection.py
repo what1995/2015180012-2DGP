4cdd079b569df696fd3667d0d64b999c45c7d1e1
2015180012박건호 <ehdrpakt@naver.com>
2018-10-26 05:18:25 +0900

    추가 화면설정
    시작화면 캐릭터선택화면 추가

---
+++ Project/test/CharacterSelection.py
@@ -0,0 +1,60 @@
+import game_framework
+from pico2d import *
+import os
+import main_state
+os.chdir('C:\\2DGP\\2015180012-2DGP\\Project\\FCGimage')
+#import CharacterSelection
+import game_world
+
+name = "CharacterSelection"
+image = None
+start = None
+character = None
+def enter():
+    global image,character
+    global start
+    image = load_image('CharacterSelection.png')
+
+def exit():
+    global image
+    global start
+    del(image)
+    del(start)
+
+
+def handle_events():
+    global character
+    events = get_events()
+    for event in events:
+        if event.type ==SDL_QUIT:
+            game_framework.quit()
+        else:
+            if(event.type, event.key) == (SDL_KEYDOWN,SDLK_ESCAPE):
+                game_framework.quit()
+            elif(event.type, event.key)==(SDL_KEYDOWN,SDLK_SPACE):
+                character=1
+                game_framework.change_state(main_state)
+
+
+def draw():
+    clear_canvas()
+    image.draw(400,300)
+    
+    update_canvas()
+
+
+
+
+
+
+def update():
+    global character
+    character = 1
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
--- Project/test/main_state.py
+++ Project/test/main_state.py
@@ -5,7 +5,7 @@ import os
 from pico2d import *
 import game_framework
 import game_world
-
+import CharacterSelection
 from iku import Iku
 from reimu import Reimu
 from tenshi import Tenshi
