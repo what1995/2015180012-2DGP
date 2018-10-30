5a45e3c3cb0b92301b28af3f141b5180deab4185
2015180012박건호 <ehdrpakt@naver.com>
2018-10-30 20:27:17 +0900

    유령같은 효과추가
    유령효과추가

--- Drill12/ghost.py
+++ Drill12/ghost.py
@@ -19,18 +19,29 @@ class Ghost:
     image = None
 
     def __init__(self, x = 400, y = 300):
+        global cheak
         if Ghost.image == None:
             Ghost.image = load_image('animation_sheet.png')
         self.x, self.y= x, y
         self.frame = 0
         self.opacify=0
-        self.set=0
+        self.set=90
+        cheak=0
 
     def draw(self):
-        self.image.opacify(0.5)
+        global cheak
+
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x + 100 * math.sin(self.set),self.y + 100 * math.cos(self.set))
+
+        if cheak< 100:
+            self.image.opacify(0.3)
+            self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x,self.y+cheak)
+            cheak +=0.5
+        else:
+            self.image.opacify(self.opacify)
+            self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x + 100 * math.sin(self.set),self.y + 100 * math.cos(self.set))
 
     def update(self):
         self.set += CIRCLE_SPEED_PPS
+        self.opacify = random.randrange(1,10)*0.1
 
