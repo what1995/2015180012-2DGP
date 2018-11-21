8875b1bb6d1c8c6120a8de5075a834529455485d
2015180012박건호 <ehdrpakt@naver.com>
2018-11-21 17:45:31 +0900

    배경음악 구성

--- Drill16/grass.py
+++ Drill16/grass.py
@@ -4,6 +4,9 @@ class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
         # fill here
+        self.bgm = load_music('football.mp3')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
 
 
     def update(self):
